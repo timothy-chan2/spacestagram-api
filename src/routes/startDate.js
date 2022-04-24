8edd6ab3d51ecbda5db2ca25d40b0a9f10c342c7
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-04-24 16:20:57 -0400

    Update environment variable name

--- src/routes/startDate.js
+++ src/routes/startDate.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 module.exports = () => {
   router.get("/:startDate", (request, response) => {
-    axios.get(`https://api.nasa.gov/planetary/apod?start_date=${request.params.startDate}&api_key=${process.env.GOV_DATA_API_KEY}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?start_date=${request.params.startDate}&api_key=${process.env.API_KEY}`)
       .then(res => response.json(res.data.reverse()));
   });
 
