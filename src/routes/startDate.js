a688993540484bdab12ee95963b140a155633a66
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-04-24 15:50:59 -0400

    Import axios so it can be used

--- src/routes/startDate.js
+++ src/routes/startDate.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const router = require("express").Router();
 
+const axios = require("axios");
+
 module.exports = () => {
   router.get("/:startDate", (request, response) => {
     axios.get(`https://api.nasa.gov/planetary/apod?start_date=${request.params.startDate}&api_key=${process.env.GOV_DATA_API_KEY}`)
-      .then(response => console.log(response.data.reverse()))
-      .catch(error => console.log(error));
+      .then(res => response.json(res.data.reverse()));
   });
 
   return router;
