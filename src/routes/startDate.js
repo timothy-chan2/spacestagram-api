5d2e161526939e3e9d0801fb86a85413a94ada80
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-05-02 23:01:56 -0400

    Update to use RESTful URL

--- src/routes/startDate.js
+++ src/routes/startDate.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 const axios = require("axios");
 
 module.exports = () => {
-  router.get("/:startDate", (request, response) => {
+  router.get("/start-dates/:startDate", (request, response) => {
     axios.get(`https://api.nasa.gov/planetary/apod?start_date=${request.params.startDate}&api_key=${process.env.API_KEY}`)
       .then(res => response.json(res.data.reverse()));
   });
